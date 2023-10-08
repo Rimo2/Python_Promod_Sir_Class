4d69ba8d8d09648b4b7e917232aafcd112a135b8
Rimo2 <mail2asis@gmail.com>
2023-10-09 01:20:50 +0530

    removed extra space

--- 6th Oct 2023/Calculator.py
+++ 6th Oct 2023/Calculator.py
@@ -1,5 +1,5 @@
 num1 = int(input('Enter the 1st no:\n'))
-num2 = int(input('Enter the 2nd no:\n '))
+num2 = int(input('Enter the 2nd no:\n'))
 
 action = input('What action you want to perform: ')
 
