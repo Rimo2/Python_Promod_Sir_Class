a26b34ed298f32fdb8ad0f0387a9014c6bde6a89
Rimo2 <mail2asis@gmail.com>
2023-10-09 01:47:32 +0530

    Optimised the home work

--- 6th Oct 2023/HW_With_For_loop.py
+++ 6th Oct 2023/HW_With_For_loop.py
@@ -5,5 +5,7 @@
 num1 = int(input('Enter the no to print its table: '))
 num2 = int(input('Enter the upper limit: '))
 
+print(f'The table of {num1} with an upper limit of {num2} is as follows:')
+
 for i in range(1,num2+1):
     print(f'{num1} x {i} = {num1 * i}')
--- 6th Oct 2023/HW_With_While_loop_6thOct.py
+++ 6th Oct 2023/HW_With_While_loop_6thOct.py
@@ -4,11 +4,11 @@
 
 num1 = int(input('Enter the no to print its table:\n'))
 num2 = int(input('Enter the upper limit:\n'))
+
+print(f'The table of {num1} with an upper limit of {num2} is as follows:')
 i = 1
-j = 1
 while i <= num2:
-    j = i * num1
-    print(f'{num1} x {i} = {j}')
+    print(f'{num1} x {i} = {num1*i}')
     i = i + 1
 
 # Instead of this - print(str(num1)+' x '+str(i)+' = '+str(j)), I used the f'' to print the desired results
